97fe82bb4ef817038ea685b7b6944617c8351197
Yannick-Beau <beauyannick@gmail.com>
2021-09-25 19:09:05 +0200

    style wip

---
+++ src/components/Main/Home/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './home.scss';
+
+// == Composant
+const Home = () => (
+  <main className="home">
+    <h1>Bienvenue sur le portfolio de Yannick Beau</h1>
+  </main>
+);
+
+// == Export
+export default Home;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 // == Import
 import Header from '../Header';
+import Home from '../Main/Home';
 import './styles.scss';
 
 // == Composant
 const Portfolio = () => (
   <div className="app">
     <Header />
+    <Home />
   </div>
 );
 
